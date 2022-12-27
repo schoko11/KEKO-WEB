6868c6ebac9a39f5a44a33f3d02cf96d12371385
schoko11 <41967358+schoko11@users.noreply.github.com>
2022-12-27 13:41:26 +0100

    Add files via upload
    new design draft

--- JS/app.js
+++ JS/app.js
@@ -1,9 +1,27 @@
+
+/*
 let kemperMidiOut = WebMidi
     .enable({sysex:true}) 
     .then(function () {
+        WebMidi.inputs.forEach(inputs => {
+            console.log("###" + inputs.manufacturer, inputs.name);
+        });
         kemperMidiOut = WebMidi.getOutputByName("WIDI Master OUT")  
-        console.log(WebMidi)
-        return kemperMidiOut
+        console.log(kemperMidiOut);
+        console.log("active");
+        //var el = document.getElementById('rignext');
+        //el.addEventListener('click', function(event) {
+        //    event.preventDefault();
+        //    kemperMidiOut.channels[1].sendControlChange(48,0);
+        //})
+        //if (el !== undefined) {
+        //    console.log("el " + Object.keys(el));
+           
+        //}
+        
+        //kemperMidiOut.channels[1].sendControlChange(48,0);
+
+        return kemperMidiOut;
     })         
     .catch(err => alert(err));
 
@@ -27,23 +45,97 @@ const sendKemperMidiOut = async (typ,par1,par2) => {
 console.log("kempermidiout " + kemperMidiOut)
 //sendKemperMidiOut('CC',48,0);
 
-//kemperMidiOut.channels[1].sendControlChange(48,0)
-
-//let idsel = document.getElementById("input1")
-//alert("idsel" + idsel.) 
-
-//eventlistener, if addeventlistener and event are not labelled 'change' values are trigger while moving
-let element = document.getElementById('input1')
-element.value = 12
-element.addEventListener('change',() => console.log('change' + element.value + element.checked))
 
 //element.addEventListener('change',() => sendKemperMidiOut('CC',48,0))
 const event = new Event('change')
 element.dispatchEvent(event)
+*/
 
-var el = document.getElementById('demo');
 
-el.addEventListener('long-press', function(e) {
-    alert("fdsfd")
-    // do something
+let modalNameMapping = {
+    lpFxAHeader: "Fx Stomp A",
+    lpFxBHeader: "Fx Stomp B",
+    lpFxCHeader: "Fx Stomp C",
+    lpFxDHeader: "Fx Stomp D",
+    lpFxXHeader: "Fx Stomp X",
+    lpFxMHeader: "Fx Stomp M",
+    lpFxYHeader: "Fx Stomp Y",
+    lpFxRHeader: "Fx Stomp R",
+    lpAmplifierHeader: "Amplifier",
+    lpEqualizerHeader: "Equalizer",
+    lpCabinetHeader: "Cabinet"
+}
+
+let myOffcanvas = document.getElementById("offCanvasBottom");
+let bsOffCanvas = new bootstrap.Offcanvas(myOffcanvas);
+
+//this is trigger from relevant longpress buttons
+function triggerLongPress() {
+     console.log("done " + this + "#" + this.id);
+    //button.removeEventListener('long-press', once);
+    offCanvasBottomLabel.innerText = "hugoooo";
+    bsOffCanvas.show();
+    //const bsCollapse = new bootstrap.Collapse(myCollapse);
+    //const toggleButton = document.getElementById(this.id);
+    //bsCollapse.show();
+    //toggleButton.addEventListener('click', () => {
+    //    bsCollapse.toggle();
+    //})
+    
+}
+
+//set gain value of rig
+//rigGain1.value = 10;
+
+let perfModeMeters = document.getElementsByClassName("perfMode");
+let rigModeMeters = document.getElementsByClassName("rigMode");
+for (rigModeMeter of rigModeMeters) { rigModeMeter.style.visibility = 'hidden';  } //hide rig gain on startup because we start in perf mode
+
+let longClickElements = document.getElementsByClassName('longPress');
+console.log(longClickElements.length);
+for(i = 0; i < longClickElements.length; i++) {    //add longpress event listeners to fx, amplifier equalizer and cabinet
+    //console.log("" + longClickElements[i].id.substring(0,1) );
+    //only buttons with id starting with "lp"
+    if (longClickElements[i].id.substring(0,2) === "lp") { 
+        longClickElements[i].addEventListener('long-press',triggerLongPress, true); 
+        longClickElements[i].addEventListener('click',triggerShortPress, true);
+    }
+}
+
+//show gain meter in rigmode aka "browser"
+document.getElementById('perform').addEventListener('hide.bs.collapse', function () {
+    for (rigModeMeter of rigModeMeters) { rigModeMeter.style.visibility = 'visible';  } //hide rig gain on startup, because we start in per mode
+ 
 });
+
+//hide gain meter when in perfmode aka "perform"
+document.getElementById('perform').addEventListener('show.bs.collapse', function () {
+    for (rigModeMeter of rigModeMeters) { rigModeMeter.style.visibility = 'hidden';  }
+});
+
+
+function triggerShortPress() {
+   console.log(document.getElementById("modalParamsHeader").setAttribute("textContent","fdsfds") );
+   modalParamsHeader.innerText = modalNameMapping[this.id + "Header"];  //set text of modal header to id "modalParamsHeader"
+}
+
+function handleKnobFixElements() {
+    console.log("handleknobfixelements " + this.id + "#" + this.value);
+    let temp = this.id;
+    document.getElementById(this.id).innerText = this.value;
+    console.log(document.getElementById("perform").className);
+    //console.log( document.getElementById("perform").offsetHeight + "#" + document.getElementById("perform").className.match(/collapse show/g) + "##" + document.getElementById("perform").visibility);
+    //this.id.innerText = this.value;
+}
+
+
+console.log("start" + window.screen.availHeight + "#" + window.screen.availWidth);
+
+
+//catch the fixed elements by class
+let inputKnobFixElements = document.getElementsByClassName("inputKnobFix");
+for (inputKnobFixElement of inputKnobFixElements) {
+    inputKnobFixElement.addEventListener("input", handleKnobFixElements, true );
+}
+
+
